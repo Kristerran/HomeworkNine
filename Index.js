d208ada9ac276643fbbc13c8b401e208d1a3a507
kristerran <kristopherbaily@gmail.com>
2021-08-30 11:26:50 -0700

    First commit, added files with basic pseudo code and a git ignore file (Test to see if gitignore successful)

---
+++ Index.js
@@ -0,0 +1,21 @@
+// Inquirer
+
+//Objects:
+// team members #1 parent
+// Needs: Manager name, Employee id, Email adress, Office number
+// Create a menu: Engineer or intern
+
+//Engineer child of team members:
+// Needs: Name, Id, Email, Github username
+
+//Intern child of team members
+// Needs: Name, Id, email, school
+
+
+// Functions
+
+
+
+//Exit function that closes menu and calls html gen
+
+//To Do: Function that creates HTML FIle.
